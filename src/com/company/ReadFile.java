347f68f7406a7c24b906074986fb176f44ad2838
manyyeon <udayeon@naver.com>
2022-05-27 04:46:22 +0900

    문자열 100개 읽기

--- src/com/company/ReadFile.java
+++ src/com/company/ReadFile.java
@@ -1,4 +1,36 @@
 package com.company;
 
-public class FileReader {
+import java.io.File;
+import java.io.*;
+import java.util.*;
+
+class ReadFile {
+    Vector<Character> v;
+    // 일단은 생성자에 구현해놨는데 나중에 바꿀거임
+    public ReadFile() {
+        v = new Vector<Character>();
+        File file = new File("./inputText/Files/File1.txt");
+        FileReader fileReader;
+        try {
+            fileReader = new FileReader(file);
+            int c = 0;
+            int j = 0;
+            while(((c = fileReader.read()) != -1)) {
+                if(j == 100) {
+                    break;
+                }
+                System.out.print((char)c);
+                v.add((char)c);
+                j++;
+            }
+            System.out.println();
+            for(int i=0; i<v.size(); i++) {
+                char x = v.get(i);
+                System.out.print(x + " ");
+            }
+            fileReader.close();
+        } catch (IOException e) {
+            System.out.println("입출력 오류");
+        }
+    }
 }
