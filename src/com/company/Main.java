96ac18fb34dfe7d4e48c0101f88166f0bbb759c4
manyyeon <udayeon@naver.com>
2022-05-28 01:20:16 +0900

    main 함수 만들기

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,25 @@
 package com.company;
 
-public class Main {
+import java.util.*;
+import java.io.*;
 
+public class Main {
     public static void main(String[] args) {
-	    MalwareTestSystem malwareTestSystem = new MalwareTestSystem();
+        int threadNum = 0; // 스레드 총 개수
+        Runnable [] runnables; // runnable 객체 배열
+        Thread [] threads; // 스레드 배열
+        MalwareTestSystem malwareTestSystem = new MalwareTestSystem();
+
+        Scanner sc = new Scanner(System.in);
+        System.out.print("스레드 개수 입력 >> ");
+        threadNum = sc.nextInt();
+
+        runnables = new Runnable[threadNum];
+        threads = new Thread[threadNum];
+
+        for(int i=0; i<threadNum; i++) {
+            runnables[i] = new MalwareTestThread(i, malwareTestSystem);
+            threads[i] = new Thread(runnables[i]);
+        }
     }
 }
